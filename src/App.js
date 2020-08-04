5d44dd656042885e333c5477408a6cdb900f03db
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-04 13:33:00 -0700

    Font awesome successfully installed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FontAwesome from 'react-fontawesome'
 
 function App() {
   return (
     <div className="container">
     <div className="row">
-    <div className="col-6"> number one </div>
+    <div className="col-6"> <FontAwesome className="fab fa-home" /> </div>
     </div>
     </div>
   );
