4c1729edbe808706ffd9e86ab927cd709199504d
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-04 19:10:08 -0700

    Created components templates and added to App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FontAwesome from 'react-fontawesome'
+import FontAwesome from 'react-fontawesome';
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+
 
 function App() {
   return (
-    <div className="container">
-    <div className="row">
-    <div className="col-6"> Hellooo<FontAwesome className="fab fa-home" /> </div>
-    </div>
-    </div>
+    <React.Fragment> 
+    <Navbar />
+    <ProductList />
+    <Details />
+    <Cart />
+    <Default />
+    </React.Fragment>
   );
 }
 
