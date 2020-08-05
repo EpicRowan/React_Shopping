b471faf9e21caa88e71984cd79e2426be2939015
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-04 22:50:42 -0700

    Created Navbar links and cart icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,16 +11,21 @@ import Cart from "./components/Cart";
 import Default from "./components/Default";
 
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <React.Fragment> 
     <Navbar />
-    <ProductList />
-    <Details />
-    <Cart />
-    <Default />
-    </React.Fragment>
+    <Switch>
+      <Route exact path="/" component={ProductList} />
+      <Route path="/details" component={Details} />
+      <Route path="/cart" component={Cart} />
+      <Route component={Default} />
+    </Switch>
+
+    < /React.Fragment>
   );
+}
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg'
+import FontAwesome from 'react-fontawesome';
+
+
 
 class Navbar extends Component {
 	render () {
 		return (
-			<div>
-				<h3> Hello from Navbar </h3>
-			</div>
+			<nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+				<Link to='/'>
+					<img src={logo} alt="" className="navbar-brand"/>
+				</Link>
+				<ul className="navbar-nav align-items-center">
+					<li className="nav-item ml-5">
+						<Link to="/" className="nav-link">
+						Products
+						</Link>
+
+					</li>
+				</ul>
+				<Link to='/cart' classname="ml-auto">
+					<button>
+						<FontAwesome className="fas fa-cart-plus" />
+						My cart
+					</button>
+				</Link>
+			</nav>
 			)
 	}
 } 
