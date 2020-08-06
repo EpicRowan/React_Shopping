b7174d5279d2fee6b0cce95fc88d8c025d77e002
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-06 11:31:49 -0700

    Successfully added cart button to product image

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import  {ProductConsumer} from '../context';
+import FontAwesome from 'react-fontawesome';
+
 
 export default class Product extends Component {
 	render () {
@@ -10,14 +12,29 @@ export default class Product extends Component {
 			<ProductWrapper className="col-9 mx-auto col-med-6 col-lg-3 my-3">
 
 				<div className="card">
-				<div className="img-container p-5" onClick={console.log("Click")
+				<div className="img-container p-5" onClick={() => console.log("Click")
 
 				}>
-				<Link to="/details">
-				<img src={img} alt="product" className="card-img-top">
-				</img>
-	
-				</Link>
+					<Link to="/details">
+						<img src={img} alt="product" className="card-img-top">
+						</img>
+					</Link>
+					
+					<button 
+						className="cart-btn" 
+						disabled={inCart ? true : false}
+						onClick={() => {
+							console.log("added");
+					     }}
+				     >
+				     {inCart ? (
+				     	<p className="text-capitalize mb-0" disabled>
+				     	{""}
+				     	in inCart
+				     	</p>
+				     	) : (<FontAwesome className="fas fa-cart-plus" />
+				     	)}
+					</button>
 				</div>
 				</div>
 			</ProductWrapper>
