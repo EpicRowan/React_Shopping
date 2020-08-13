773d783f3e70b86bf4a37a8d2760797dbab1f360
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-13 02:59:43 -0700

    Cleaned up code in button file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
+
 import styled from 'styled-components'
+
+//With styled components, can pass props in directly
 
 export const ButtonContainer = styled.button`
 	text-transform:capitalize;
 	font-size:1.4rem;
 	background:transparent;
 	border:0.05rem solid var(--lightBlue);
-	color:${prop => prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+	color:${props => props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
 	border-radius:0.5rem;
 	border-color:${props => props.cart? "var(--mainYellow)" : "var(--lightBlue)"};
 	padding:0.2rem 0.5rem;
@@ -13,7 +16,7 @@ export const ButtonContainer = styled.button`
 	margin:0.2rem 0.5rem 0.2rem 0;
 	transition:all 0.5s ease-in-out;
     &:hover {
-		background: ${prop => prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+		background: ${props => props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
 		color:var(--mainBlue);
 
 	}
